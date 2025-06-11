9e7fd1892224efcd66b4d827ca053ccff3edef6b
jeongan327 <95003998+jeongan327@users.noreply.github.com>
2025-06-11 21:44:38 +0900

    Update CustomOAuth2UserService.java

--- src/main/java/kr/ac/dankook/cs/curation/login/service/CustomOAuth2UserService.java
+++ src/main/java/kr/ac/dankook/cs/curation/login/service/CustomOAuth2UserService.java
@@ -26,7 +26,7 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        // 1) OAuth2 공급자에서 사용자 정보 조회
+        // OAuth2 공급자에서 사용자 정보 조회
         OAuth2User oauthUser = super.loadUser(userRequest);
 
         String registrationId = userRequest.getClientRegistration().getRegistrationId(); // "google" or "kakao"
@@ -35,25 +35,27 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
                                             .getUserInfoEndpoint()
                                             .getUserNameAttributeName();
 
+        // oauth2 관련 정보를 저장할 객체
         OAuthAttributes attrs = OAuthAttributes.of(registrationId, userNameAttr, oauthUser.getAttributes());
 
+        // 입력된 정보로 검색 - 없으면 생성
         User user = userRepository.findByLoginId(attrs.getEmail())
             .orElseGet(() -> userRepository.save(attrs.toEntity()));
 
+        // 토큰 발급
         String accessToken  = jwtProvider.createAccessToken(user.getLoginId());
         String refreshToken = jwtProvider.createRefreshToken(user.getLoginId());
 
-        // 5) Refresh 토큰과 만료시간을 DB에 저장
         user.setRefreshToken(refreshToken);
         user.setRefreshTokenExpiry(new Date(System.currentTimeMillis() + jwtProvider.getRefreshExpMs()));
         userRepository.save(user);
 
-        // 6) OAuth2User attributes 맵에 JWT 토큰 추가
+        // OAuth2User attributes 맵에 JWT 토큰 추가
         Map<String,Object> attributes = new HashMap<>(oauthUser.getAttributes());
         attributes.put("accessToken",  accessToken);
         attributes.put("refreshToken", refreshToken);
 
-        // 7) Spring Security에 사용할 DefaultOAuth2User 반환
+        // Spring Security에 사용할 DefaultOAuth2User 반환
         return new DefaultOAuth2User(
             Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")),
             attributes,
